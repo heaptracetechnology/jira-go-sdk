fc33048cf7ebe28d27875da5a46ca6371ddec9fb
ht68rohit <rohits@heaptrace.com>
2019-07-19 20:02:41 +0530

    Added create project struct

--- project.go
+++ project.go
@@ -41,7 +41,7 @@ type Project struct {
 	ID              string             `json:"id,omitempty" structs:"id,omitempty"`
 	Key             string             `json:"key,omitempty" structs:"key,omitempty"`
 	Description     string             `json:"description,omitempty" structs:"description,omitempty"`
-	Lead            User               `json:"leadDetails,omitempty" structs:"leadDetails,omitempty"`
+	Lead            User               `json:"lead,omitempty" structs:"lead,omitempty"`
 	Components      []ProjectComponent `json:"components,omitempty" structs:"components,omitempty"`
 	IssueTypes      []IssueType        `json:"issueTypes,omitempty" structs:"issueTypes,omitempty"`
 	URL             string             `json:"url,omitempty" structs:"url,omitempty"`
@@ -51,9 +51,27 @@ type Project struct {
 	Name            string             `json:"name,omitempty" structs:"name,omitempty"`
 	Roles           map[string]string  `json:"roles,omitempty" structs:"roles,omitempty"`
 	AvatarUrls      AvatarUrls         `json:"avatarUrls,omitempty" structs:"avatarUrls,omitempty"`
-	ProjectCategory ProjectCategory    `json:"projectCategory,omitempty" structs:"projectCategory,omitempty"`
-	ProjectTypeKey  string    	   `json:"projectTypeKey,omitempty" structs:"projectTypeKey,omitempty"`
-	ProjectLead            string      `json:"lead,omitempty"`
+	ProjectCategory ProjectCategory    `json:"projectCategory,omitempty" structs:"projectCategory,omitempty"`	
+}
+
+// CreateProject represents a JIRA CreateProject Struct.
+type CreateProject struct {
+	Expand      string `json:"expand,omitempty" structs:"expand,omitempty"`
+	Self        string `json:"self,omitempty" structs:"self,omitempty"`
+	ID          string `json:"id,omitempty" structs:"id,omitempty"`
+	Key         string `json:"key,omitempty" structs:"key,omitempty"`
+	Description string `json:"description,omitempty" structs:"description,omitempty"`	
+	Components   []ProjectComponent `json:"components,omitempty" structs:"components,omitempty"`
+	IssueTypes   []IssueType        `json:"issueTypes,omitempty" structs:"issueTypes,omitempty"`
+	URL          string             `json:"url,omitempty" structs:"url,omitempty"`
+	Email        string             `json:"email,omitempty" structs:"email,omitempty"`
+	AssigneeType string             `json:"assigneeType,omitempty" structs:"assigneeType,omitempty"`
+	Versions     []Version          `json:"versions,omitempty" structs:"versions,omitempty"`
+	Name         string             `json:"name,omitempty" structs:"name,omitempty"`
+	Roles        map[string]string  `json:"roles,omitempty" structs:"roles,omitempty"`
+	ProjectCategory ProjectCategory `json:"projectCategory,omitempty" structs:"projectCategory,omitempty"`
+	ProjectTypeKey  string          `json:"projectTypeKey,omitempty" structs:"projectTypeKey,omitempty"`
+	Lead            string          `json:"lead,omitempty" structs:"lead,omitempty"`
 }
 
 // ProjectComponent represents a single component of a project
